af23875a6b7ec51c17c95e1008bdb8ae4b66cfbf
trashbyte <github@trashbyte.io>
2021-11-15 15:02:00 -0800

    added reference QEMU raspi3 system pending aarch64 testing

---
+++ pi3b+/run rpios.bat
@@ -0,0 +1,2 @@
+qemu-system-aarch64 -M raspi3 -cpu arm1176 -m 1G -smp 4 -sd "2021-10-30-raspios-bullseye-armhf-lite.img" -dtb bcm2710-rpi-3-b-plus.dtb -kernel kernel8.img -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1" -serial stdio -usb -device usb-mouse -device usb-kbd
+pause
